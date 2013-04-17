161a108d4f7ee3a3f01163b52095edf5d6c55b2e
Kouhei Sutou <kou@clear-code.com>
2013-04-17 14:04:01 +0900

    Revert "travis: show git --version instead"
    This reverts commit 7b5d57472bf93c90ae5761d0d7fb24de973758fa.
    
    Git on Travis is 1.8.2. It is the cause of test failure on Travis!
    
        git version 1.8.2

--- test/run-test.rb
+++ test/run-test.rb
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-system("git --version")
+system("git config --list")
 
 $VERBOSE = true
 
