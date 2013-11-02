222f5e3b0d0a05f4d4b352d38851d7f18183238f
okkez <okkez000@gmail.com>
2013-11-03 00:06:52 +0900

    Fix indent

--- github-post-receiver/test/receiver-test.rb
+++ github-post-receiver/test/receiver-test.rb
@@ -70,222 +70,222 @@ class ReceiverTest < Test::Unit::TestCase
   end
 
   class GitHubTest < self
-  def test_post_with_non_target_repository
-    owner_name = "devil"
-    repository_name = "evil-repository"
-    post_payload(:repository => {
-                   :name => repository_name,
-                   :url => "https://github.com/super-devil/evil-repository",
-                   :owner => {
-                     :name => owner_name,
-                   },
-                 })
-    assert_response("Forbidden")
-    assert_equal("unacceptable repository: " +
-                 "<#{owner_name.inspect}>:<#{repository_name.inspect}>",
-                 response_body)
-  end
+    def test_post_with_non_target_repository
+      owner_name = "devil"
+      repository_name = "evil-repository"
+      post_payload(:repository => {
+                     :name => repository_name,
+                     :url => "https://github.com/super-devil/evil-repository",
+                     :owner => {
+                       :name => owner_name,
+                     },
+                   })
+      assert_response("Forbidden")
+      assert_equal("unacceptable repository: " +
+                   "<#{owner_name.inspect}>:<#{repository_name.inspect}>",
+                   response_body)
+    end
 
-  def test_post_without_owner
-    repository = {
-      "url" => "https://github.com/ranguba/rroonga",
-      "name" => "rroonga",
-    }
-    payload = {
-      "repository" => repository,
-    }
-    post_payload(payload)
-    assert_response("Bad Request")
-    assert_equal("repository owner is missing: <#{repository.inspect}>",
-                 response_body)
-  end
-
-  def test_post_without_owner_name
-    repository = {
-      "url" => "https://github.com/ranguba/rroonga",
-      "name" => "rroonga",
-      "owner" => {},
-    }
-    payload = {
-      "repository" => repository,
-    }
-    post_payload(payload)
-    assert_response("Bad Request")
-    assert_equal("repository owner name is missing: <#{repository.inspect}>",
-                 response_body)
-  end
-
-  def test_post_without_before
-    payload = {
-      "repository" => {
+    def test_post_without_owner
+      repository = {
         "url" => "https://github.com/ranguba/rroonga",
         "name" => "rroonga",
-        "owner" => {
-          "name" => "ranguba",
+      }
+      payload = {
+        "repository" => repository,
+      }
+      post_payload(payload)
+      assert_response("Bad Request")
+      assert_equal("repository owner is missing: <#{repository.inspect}>",
+                   response_body)
+    end
+
+    def test_post_without_owner_name
+      repository = {
+        "url" => "https://github.com/ranguba/rroonga",
+        "name" => "rroonga",
+        "owner" => {},
+      }
+      payload = {
+        "repository" => repository,
+      }
+      post_payload(payload)
+      assert_response("Bad Request")
+      assert_equal("repository owner name is missing: <#{repository.inspect}>",
+                   response_body)
+    end
+
+    def test_post_without_before
+      payload = {
+        "repository" => {
+          "url" => "https://github.com/ranguba/rroonga",
+          "name" => "rroonga",
+          "owner" => {
+            "name" => "ranguba",
+          },
+        }
+      }
+      post_payload(payload)
+      assert_response("Bad Request")
+      assert_equal("before commit ID is missing: <#{payload.inspect}>",
+                   response_body)
+    end
+
+    def test_post_without_after
+      payload = {
+        "before" => "0f2be32a3671360a323f1dee64c757bc9fc44998",
+        "repository" => {
+          "url" => "https://github.com/ranguba/rroonga",
+          "name" => "rroonga",
+          "owner" => {
+            "name" => "ranguba",
+          },
         },
       }
-    }
-    post_payload(payload)
-    assert_response("Bad Request")
-    assert_equal("before commit ID is missing: <#{payload.inspect}>",
-                 response_body)
-  end
+      post_payload(payload)
+      assert_response("Bad Request")
+      assert_equal("after commit ID is missing: <#{payload.inspect}>",
+                   response_body)
+    end
 
-  def test_post_without_after
-    payload = {
-      "before" => "0f2be32a3671360a323f1dee64c757bc9fc44998",
-      "repository" => {
-        "url" => "https://github.com/ranguba/rroonga",
-        "name" => "rroonga",
-        "owner" => {
-          "name" => "ranguba",
+    def test_post_without_reference
+      payload = {
+        "before" => "0f2be32a3671360a323f1dee64c757bc9fc44998",
+        "after" => "c7bf92799225d67788be7c42ea4f504a47708390",
+        "repository" => {
+          "url" => "https://github.com/ranguba/rroonga",
+          "name" => "rroonga",
+          "owner" => {
+            "name" => "ranguba",
+          },
         },
-      },
-    }
-    post_payload(payload)
-    assert_response("Bad Request")
-    assert_equal("after commit ID is missing: <#{payload.inspect}>",
-                 response_body)
-  end
-
-  def test_post_without_reference
-    payload = {
-      "before" => "0f2be32a3671360a323f1dee64c757bc9fc44998",
-      "after" => "c7bf92799225d67788be7c42ea4f504a47708390",
-      "repository" => {
-        "url" => "https://github.com/ranguba/rroonga",
-        "name" => "rroonga",
-        "owner" => {
-          "name" => "ranguba",
-        },
-      },
-    }
-    post_payload(payload)
-    assert_response("Bad Request")
-    assert_equal("reference is missing: <#{payload.inspect}>",
-                 response_body)
-  end
-
-  def test_post
-    repository_mirror_path = mirror_path("ranguba", "rroonga")
-    assert_false(File.exist?(repository_mirror_path))
-    before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
-    after = "c7bf92799225d67788be7c42ea4f504a47708390"
-    reference = "refs/heads/master"
-    post_payload(:repository => {
-                   :url => "https://github.com/ranguba/rroonga",
-                   :name => "rroonga",
-                   :owner => {
-                     :name => "ranguba",
-                   },
-                 },
-                 :before => before,
-                 :after => after,
-                 :ref => reference)
-    assert_response("OK")
-    assert_true(File.exist?(repository_mirror_path))
-    result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
-    assert_equal([{
-                    "argv" => ["--repository", repository_mirror_path,
-                               "--max-size", "1M",
-                               "--repository-browser", "github",
-                               "--github-user", "ranguba",
-                               "--github-repository", "rroonga",
-                               "--name", "ranguba/rroonga",
-                               "null@example.com"],
-                    "lines" => ["#{before} #{after} #{reference}\n"],
-                  }],
-                 result)
-  end
-
-  def test_per_owner_configuration
-    repository_mirror_path = mirror_path("ranguba", "rroonga")
-    assert_false(File.exist?(repository_mirror_path))
-    options[:owners] = {
-      "ranguba" => {
-        :to => "ranguba-commit@example.org",
-        :from => "ranguba+commit@example.org",
-        :sender => "null@example.org",
       }
-    }
-    before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
-    after = "c7bf92799225d67788be7c42ea4f504a47708390"
-    reference = "refs/heads/master"
-    post_payload(:repository => {
-                   :url => "https://github.com/ranguba/rroonga",
-                   :name => "rroonga",
-                   :owner => {
-                     :name => "ranguba",
-                   },
-                 },
-                 :before => before,
-                 :after => after,
-                 :ref => reference)
-    assert_response("OK")
-    assert_true(File.exist?(repository_mirror_path))
-    result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
-    assert_equal([{
-                    "argv" => ["--repository", repository_mirror_path,
-                               "--max-size", "1M",
-                               "--repository-browser", "github",
-                               "--github-user", "ranguba",
-                               "--github-repository", "rroonga",
-                               "--name", "ranguba/rroonga",
-                               "--from", "ranguba+commit@example.org",
-                               "--sender", "null@example.org",
-                               "ranguba-commit@example.org"],
-                    "lines" => ["#{before} #{after} #{reference}\n"],
-                  }],
-                 result)
-  end
+      post_payload(payload)
+      assert_response("Bad Request")
+      assert_equal("reference is missing: <#{payload.inspect}>",
+                   response_body)
+    end
 
-  def test_per_repository_configuration
-    repository_mirror_path = mirror_path("ranguba", "rroonga")
-    assert_false(File.exist?(repository_mirror_path))
-    options[:owners] = {
-      "ranguba" => {
-        :to => "ranguba-commit@example.org",
-        :from => "ranguba+commit@example.org",
-        :sender => "null@example.org",
-        "repositories" => {
-          "rroonga" => {
-            :to => "ranguba-commit@example.net",
-            :from => "ranguba+commit@example.net",
-            :sender => "null@example.net",
+    def test_post
+      repository_mirror_path = mirror_path("ranguba", "rroonga")
+      assert_false(File.exist?(repository_mirror_path))
+      before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
+      after = "c7bf92799225d67788be7c42ea4f504a47708390"
+      reference = "refs/heads/master"
+      post_payload(:repository => {
+                     :url => "https://github.com/ranguba/rroonga",
+                     :name => "rroonga",
+                     :owner => {
+                       :name => "ranguba",
+                     },
+                   },
+                   :before => before,
+                   :after => after,
+                   :ref => reference)
+      assert_response("OK")
+      assert_true(File.exist?(repository_mirror_path))
+      result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
+      assert_equal([{
+                       "argv" => ["--repository", repository_mirror_path,
+                         "--max-size", "1M",
+                         "--repository-browser", "github",
+                         "--github-user", "ranguba",
+                         "--github-repository", "rroonga",
+                         "--name", "ranguba/rroonga",
+                         "null@example.com"],
+                       "lines" => ["#{before} #{after} #{reference}\n"],
+                     }],
+                   result)
+    end
+
+    def test_per_owner_configuration
+      repository_mirror_path = mirror_path("ranguba", "rroonga")
+      assert_false(File.exist?(repository_mirror_path))
+      options[:owners] = {
+        "ranguba" => {
+          :to => "ranguba-commit@example.org",
+          :from => "ranguba+commit@example.org",
+          :sender => "null@example.org",
+        }
+      }
+      before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
+      after = "c7bf92799225d67788be7c42ea4f504a47708390"
+      reference = "refs/heads/master"
+      post_payload(:repository => {
+                     :url => "https://github.com/ranguba/rroonga",
+                     :name => "rroonga",
+                     :owner => {
+                       :name => "ranguba",
+                     },
+                   },
+                   :before => before,
+                   :after => after,
+                   :ref => reference)
+      assert_response("OK")
+      assert_true(File.exist?(repository_mirror_path))
+      result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
+      assert_equal([{
+                       "argv" => ["--repository", repository_mirror_path,
+                         "--max-size", "1M",
+                         "--repository-browser", "github",
+                         "--github-user", "ranguba",
+                         "--github-repository", "rroonga",
+                         "--name", "ranguba/rroonga",
+                         "--from", "ranguba+commit@example.org",
+                         "--sender", "null@example.org",
+                         "ranguba-commit@example.org"],
+                       "lines" => ["#{before} #{after} #{reference}\n"],
+                     }],
+                   result)
+    end
+
+    def test_per_repository_configuration
+      repository_mirror_path = mirror_path("ranguba", "rroonga")
+      assert_false(File.exist?(repository_mirror_path))
+      options[:owners] = {
+        "ranguba" => {
+          :to => "ranguba-commit@example.org",
+          :from => "ranguba+commit@example.org",
+          :sender => "null@example.org",
+          "repositories" => {
+            "rroonga" => {
+              :to => "ranguba-commit@example.net",
+              :from => "ranguba+commit@example.net",
+              :sender => "null@example.net",
+            }
           }
         }
       }
-    }
-    before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
-    after = "c7bf92799225d67788be7c42ea4f504a47708390"
-    reference = "refs/heads/master"
-    post_payload(:repository => {
-                   :url => "https://github.com/ranguba/rroonga",
-                   :name => "rroonga",
-                   :owner => {
-                     :name => "ranguba",
+      before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
+      after = "c7bf92799225d67788be7c42ea4f504a47708390"
+      reference = "refs/heads/master"
+      post_payload(:repository => {
+                     :url => "https://github.com/ranguba/rroonga",
+                     :name => "rroonga",
+                     :owner => {
+                       :name => "ranguba",
+                     },
                    },
-                 },
-                 :before => before,
-                 :after => after,
-                 :ref => reference)
-    assert_response("OK")
-    assert_true(File.exist?(repository_mirror_path))
-    result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
-    assert_equal([{
-                    "argv" => ["--repository", repository_mirror_path,
-                               "--max-size", "1M",
-                               "--repository-browser", "github",
-                               "--github-user", "ranguba",
-                               "--github-repository", "rroonga",
-                               "--name", "ranguba/rroonga",
-                               "--from", "ranguba+commit@example.net",
-                               "--sender", "null@example.net",
-                               "ranguba-commit@example.net"],
-                    "lines" => ["#{before} #{after} #{reference}\n"],
-                  }],
-                 result)
-  end
+                   :before => before,
+                   :after => after,
+                   :ref => reference)
+      assert_response("OK")
+      assert_true(File.exist?(repository_mirror_path))
+      result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
+      assert_equal([{
+                       "argv" => ["--repository", repository_mirror_path,
+                         "--max-size", "1M",
+                         "--repository-browser", "github",
+                         "--github-user", "ranguba",
+                         "--github-repository", "rroonga",
+                         "--name", "ranguba/rroonga",
+                         "--from", "ranguba+commit@example.net",
+                         "--sender", "null@example.net",
+                         "ranguba-commit@example.net"],
+                       "lines" => ["#{before} #{after} #{reference}\n"],
+                     }],
+                   result)
+    end
   end
 
   private
