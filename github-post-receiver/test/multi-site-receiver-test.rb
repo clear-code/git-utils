bd31abedd009056222adfb4431c1ebbc2c5a17d6
Kenji Okimoto <okimoto@clear-code.com>
2013-11-03 14:04:58 +0900

    Use data-driven style and add test cases for GitLab site

--- github-post-receiver/test/multi-site-receiver-test.rb
+++ github-post-receiver/test/multi-site-receiver-test.rb
@@ -39,17 +39,104 @@ class MultiSiteReceiverTest < Test::Unit::TestCase
   end
 
   class GitLabTest < self
-    def test_post
-      gitlab_project_uri = "https://gitlab.example.com/ranguba/rroonga"
-      repository_mirror_path = mirror_path("ranguba", "rroonga")
+    data("gitlab.example.com/ranguba/rroonga" => {
+           :params => {
+             :domain => "gitlab.example.org",
+             :owner_name => "ranguba",
+             :repository_name => "rroonga"
+           },
+           :expected => {
+             :sender => "sender@example.com",
+             :error_to => "error@example.com",
+             :to => "global-to@example.com",
+           }
+         },
+         "gitlab.example.net/clear-code/git-utils" => {
+           :params => {
+             :domain => "gitlab.example.net",
+             :owner_name => "clear-code",
+             :repository_name => "git-utils"
+           },
+           :expected => {
+             :add_html => true,
+             :sender => "sender@example.com",
+             :error_to => "error@example.com",
+             :to => "commit@example.net",
+           }
+         },
+         "gitlab.example.net/support/git-utils" => {
+           :params => {
+             :domain => "gitlab.example.net",
+             :owner_name => "support",
+             :repository_name => "git-utils"
+           },
+           :expected => {
+             :add_html => false,
+             :from => "support+null@example.net",
+             :sender => "sender@example.com",
+             :error_to => "error@example.com",
+             :to => "support@example.net",
+           }
+         },
+         "gitlab.example.org/clear-code/test-project1" => {
+           :params => {
+             :domain => "gitlab.example.org",
+             :owner_name => "clear-code",
+             :repository_name => "test-project1"
+           },
+           :expected => {
+             :add_html => true,
+             :from => "null@example.org",
+             :sender => "sender@example.com",
+             :error_to => "error@example.com",
+             :to => "commit+test-project1@example.org",
+           }
+         },
+         "gitlab.example.org/clear-code/test-project2" => {
+           :params => {
+             :domain => "gitlab.example.org",
+             :owner_name => "clear-code",
+             :repository_name => "test-project2"
+           },
+           :expected => {
+             :add_html => false,
+             :from => "null@example.org",
+             :sender => "sender@example.com",
+             :error_to => "error@example.com",
+             :to => "commit+test-project2@example.org",
+           }
+         })
+    def test_post(data)
+      domain = data[:params][:domain]
+      owner_name = data[:params][:owner_name]
+      repository_name = data[:params][:repository_name]
+      gitlab_project_uri = "https://#{domain}/#{owner_name}/#{repository_name}"
+      repository_uri = "git@#{domain}:#{owner_name}/#{repository_name}.git"
+      repository_mirror_path = mirror_path(owner_name, repository_name)
+      add_html = data[:expected][:add_html]
+      from = data[:expected][:from]
+      sender = data[:expected][:sender]
+      error_to = data[:expected][:error_to]
+      to = data[:expected][:to]
       assert_false(File.exist?(repository_mirror_path))
       before = "0f2be32a3671360a323f1dee64c757bc9fc44998"
       after = "c7bf92799225d67788be7c42ea4f504a47708390"
       reference = "refs/heads/master"
+      expected_argv = [
+        "--repository", repository_mirror_path,
+        "--max-size", "1M",
+        "--repository-browser", "gitlab",
+        "--gitlab-project-uri", gitlab_project_uri
+      ]
+      expected_argv.push("--from", from) if from
+      expected_argv.push("--sender", sender)
+      expected_argv.push("--add-html") if add_html
+      expected_argv.push("--error-to", error_to)
+      expected_argv.push(to)
       post_payload(:repository => {
                      :homepage => gitlab_project_uri,
-                     :url => "git@gitlab.example.com:ranguba/rroonga.git",
-                     :name => "rroonga",
+                     :url => repository_uri,
+                     :name => repository_name,
                    },
                    :before => before,
                    :after => after,
@@ -59,15 +146,7 @@ class MultiSiteReceiverTest < Test::Unit::TestCase
       assert_true(File.exist?(repository_mirror_path), repository_mirror_path)
       result = YAML.load_file(File.join(@tmp_dir, "commit-email-result.yaml"))
       assert_equal([{
-                       "argv" => [
-                         "--repository", repository_mirror_path,
-                         "--max-size", "1M",
-                         "--repository-browser", "gitlab",
-                         "--gitlab-project-uri", gitlab_project_uri,
-                         "--sender", "sender@example.com",
-                         "--error-to", "error@example.com",
-                         "global-to@example.com"
-                       ],
+                       "argv" => expected_argv,
                        "lines" => ["#{before} #{after} #{reference}\n"],
                      }],
                    result)
