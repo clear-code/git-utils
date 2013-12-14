c97425ef2d66807035b8d0b8fc2424e6653ceb72
Kouhei Sutou <kou@clear-code.com>
2013-12-14 23:37:29 +0900

    Use " (double quote) for string literal

--- github-post-receiver/test/run-test.rb
+++ github-post-receiver/test/run-test.rb
@@ -21,13 +21,13 @@ base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 lib_dir = File.join(base_dir, "lib")
 test_dir = File.join(base_dir, "test")
 
-require 'test-unit'
+require "test-unit"
 
 ARGV.unshift("--priority-mode")
 
 $LOAD_PATH.unshift(lib_dir)
 
 $LOAD_PATH.unshift(test_dir)
-require 'github-post-receiver-test-utils'
+require "github-post-receiver-test-utils"
 
 exit Test::Unit::AutoRunner.run(true, test_dir)
